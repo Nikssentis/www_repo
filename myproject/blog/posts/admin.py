a9eedbae1080e9355ea1c18008990bce85e892b2
Nikssentis <andrzej.grak@gmail.com>
2025-10-10 11:07:25 +0200

    lab_02: stan na koniec dnia

--- myproject/blog/posts/admin.py
+++ myproject/blog/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Topic
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'created')
+    list_filter = ('category',)
+    search_fields = ('name',)
--- myproject/blog/posts/models.py
+++ myproject/blog/posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)  # do zadania
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+# posts/models.py
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"  # ładna liczba mnoga w adminie
+
+    def __str__(self):
+        return self.name
