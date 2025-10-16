5986162f817d9d587186c3ed914db36878ac8143
Nikssentis <andrzej.grak@gmail.com>
2025-10-16 22:51:41 +0200

    lab2

--- myproject/blog/posts/models.py
+++ myproject/blog/posts/models.py
@@ -2,25 +2,19 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
-    description = models.TextField(blank=True)  # do zadania
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    category = models.ForeignKey('posts.Category', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-# posts/models.py
-class Category(models.Model):
-    name = models.CharField(max_length=60)
-    description = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name_plural = "Categories"  # ładna liczba mnoga w adminie
 
     def __str__(self):
         return self.name
